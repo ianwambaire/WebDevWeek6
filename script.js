adab482ab2691a1195e496c160ac6e4fff893311
ianwambaire <ian.wambaire@strathmore.edu>
2025-01-25 14:35:15 +0300

    updated the landing page

---
+++ script.js
@@ -0,0 +1,7 @@
+let isBlue = true;
+function changebackgroundcolour(){
+    document.body.style.backgroundColor = isBlue ? '#f4f4f4' : '#007bff';
+    document.main.style.backgroundColor=isred ? 'black' : '#007bff';
+            isBlue = !isBlue;
+
+}
